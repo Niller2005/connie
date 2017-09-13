33cab675e8512a9ea9eb3a570d877e9740b613d2
Jacob Nielsen <jnillern@gmail.com>
2017-09-13 12:38:14 +0200

    php.sh

---
+++ php.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+sudo apt-get install -y nginx
+sudo apt-get install -y php7.0 php7.0-fpm php7.0-cli php7.0-common php7.0-mbstring php7.0-gd php7.0-intl php7.0-xml php7.0-mysql php7.0-mcrypt php7.0-zip php7.0-curl
